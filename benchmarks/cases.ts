f720effbfd65db2aa9ab4b5e5c7fd7a2c565d60b
Simone Sanfratello <simone.sanfra@gmail.com>
2025-03-18 15:30:08 +0100

    wip

--- benchmarks/cases.ts
+++ benchmarks/cases.ts
@@ -249,25 +249,25 @@ export const cases: Case[] = [
       }
     ]
   },
-  {
-    label: 'skipped due to response size (1Mb)',
-    request: {
-      method: 'GET',
-      path: '/image.jpg',
-      headers: {
-        Accept: 'image/jpeg',
-      }
-    },
-    responses: [
-      {
-        code: 200,
-        headers: {
-          'Content-Type': 'image/jpeg',
-          'Content-Length': '1048576'
-        },
-        body: Buffer.from(new Uint8Array(1024 * 1024))
-      }
-    ]
-  }
+  // {
+  //   label: 'skipped due to response size (1Mb)',
+  //   request: {
+  //     method: 'GET',
+  //     path: '/image.jpg',
+  //     headers: {
+  //       Accept: 'image/jpeg',
+  //     }
+  //   },
+  //   responses: [
+  //     {
+  //       code: 200,
+  //       headers: {
+  //         'Content-Type': 'image/jpeg',
+  //         'Content-Length': '1048576'
+  //       },
+  //       body: Buffer.from(new Uint8Array(1024 * 1024))
+  //     }
+  //   ]
+  // }
 
 ]
